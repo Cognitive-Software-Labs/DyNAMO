529e74cdfedb2bb48e0598cf51fd3920e95262df
PrathamRathod14 <i.m.pratham01@gmail.com>
2026-03-24 12:54:26 +0100

    Clean up extra blank line in run script

--- run_minimal.sh
+++ run_minimal.sh
@@ -7,5 +7,4 @@ source /opt/ros/jazzy/setup.bash
 colcon build --packages-select dynamo_minimal_sim --symlink-install
 source install/setup.bash
 
-
 ros2 launch dynamo_minimal_sim minimal_mapping_launch.py
